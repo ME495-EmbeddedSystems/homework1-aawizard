9918ad4edb07e94e6d5cf1735eeb120eb18c2623
aawizard <agwananyaa2001@gmail.com>
2023-10-03 16:09:08 -0500

    subscriber created that reads turtle pose

--- turtle_control/turtle_control/waypoint.py
+++ turtle_control/turtle_control/waypoint.py
@@ -6,6 +6,7 @@ from enum import Enum, auto
 from std_msgs.msg import String
 import numpy as np
 from turtlesim.srv import SetPen, TeleportAbsolute
+from turtlesim.msg import Pose
 import asyncio
 from rclpy.callback_groups import  ReentrantCallbackGroup
 
@@ -21,15 +22,23 @@ class Waypoint(Node):
         super().__init__('waypoint')
         self.declare_parameter('frequency', 1) 
         self.client_cb_group = ReentrantCallbackGroup()
+     
         self.freq       = self.get_parameter('frequency').get_parameter_value().double_value
+    #creating services  
         self.toggle     = self.create_service(Empty, 'toggle', self.toggle_callback)
         self.load       = self.create_service(Waypoints, 'load', self.load_callback)
-        self.timer      = self.create_timer(self.freq, self.timer_callback)
-        self.state      = State.STOPPED   
+    #creating clients
         self.reset      = self.create_client(Empty, "reset",callback_group=self.client_cb_group)
         self.setpen     = self.create_client(SetPen,'turtle1/set_pen',callback_group=self.client_cb_group)
         self.teleabs    = self.create_client(TeleportAbsolute,'turtle1/teleport_absolute',callback_group=self.client_cb_group)
-        
+    #state of the node set as stopped 
+        self.state      = State.STOPPED   
+    #Timer created        
+        self.timer      = self.create_timer(self.freq, self.timer_callback)
+    #creating a subscriber
+        self.subscription = self.create_subscription(Pose,'/turtle1/pose',self.listener_callback,10)   
+        self.pose=Pose
+  
         if not self.reset.wait_for_service(timeout_sec=1.0):
             raise RuntimeError('Timeout waiting for "reset" service to become available')
         
@@ -39,6 +48,11 @@ class Waypoint(Node):
         if not self.teleabs.wait_for_service(timeout_sec=1.0):
             raise RuntimeError('Timeout waiting for "teleport_absolute" service to become available')
         
+    
+    
+    
+    
+    
     
     async def draw_x(self,x,y):
         #pen up
@@ -77,7 +91,7 @@ class Waypoint(Node):
         
         
         
-           
+    #callback for load service     
     async def  load_callback(self,request,response):
         await self.reset.call_async(Empty.Request())
         
@@ -89,8 +103,11 @@ class Waypoint(Node):
             await self.draw_x(point.x,point.y)     #Drawing X at each waypoint
         
         #take the turtle to first waypoint
-        x=request.wpoint[0].x
-        y=request.wpoint[0].y
+        # x=request.wpoint[0].x
+        # y=request.wpoint[0].y
+        
+        x=wpoints[0][0]
+        y=wpoints[0][1]
         await self.teleabs.call_async(TeleportAbsolute.Request(x=x,y=y))
         self.state      = State.STOPPED 
         
@@ -98,7 +115,9 @@ class Waypoint(Node):
         response.dist = self.cal_dist(wpoints=wpoints)  #calculatinf straight line distance
         # response.dist= 2.0
         return response
-        
+    
+    
+    #callback for toggle service
     def toggle_callback(self, request, response):
         if self.state==State.MOVING:
             self.state=State.STOPPED
@@ -107,14 +126,17 @@ class Waypoint(Node):
             self.state=State.MOVING
         return response
     
+    #callback for subscriber
+    def listener_callback(self, msg):
+        self.pose=msg
     
+    #callback for timmer
     def timer_callback(self):
         msg = String()
         if self.state==State.MOVING:
             msg.data = 'Issuing Command!'
             self.get_logger().debug('Publishing: "%s"' % msg.data)
-            
-        
+
 
 
 
